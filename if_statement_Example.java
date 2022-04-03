5320b6f9173a8a626e6018712796436204bdbe98
AMIT RAJBHAR <rajbharamit38@gmail.com>
2022-04-04 01:23:37 +0530

    conditional Statement Examples

---
+++ if_statement_Example.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/*  a) if statement
+    b) nested if statement
+    c) if-else statement
+    d) if-else-if statement
+   */
+
+    
+        // if statement
+
+public class if_statement_Example {
+    public static void main(String[] args)
+    {
+        System.out.print("Enter Your Number: ");
+        Scanner s=new Scanner(System.in);
+        int num=s.nextInt();
+        if (num<100)
+        {
+            System.out.println(num+" is then Less than 100");
+        }
+    }    
+}
+        
+        
+
+
+ 
