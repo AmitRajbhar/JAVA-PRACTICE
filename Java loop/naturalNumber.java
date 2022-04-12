4b7e9bce209827f1244988b879e36e6553cc14b1
AMIT RAJBHAR <rajbharamit38@gmail.com>
2022-04-13 03:52:58 +0530

    Natural Number

---
+++ Java loop/naturalNumber.java
@@ -0,0 +1,12 @@
+public class naturalNumber {
+    public static void main(String[] args)
+    {
+        int num=0;
+        while(num<10)
+        {
+            num++;
+            System.out.println(num);
+        }
+    }
+    
+}
