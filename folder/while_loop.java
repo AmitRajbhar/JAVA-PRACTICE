82c46db69e21780040f41ce981c5e7a980a0a80e
AMIT RAJBHAR <rajbharamit38@gmail.com>
2022-04-13 03:31:48 +0530

    01

---
+++ folder/while_loop.java
@@ -0,0 +1,28 @@
+package folder;
+
+import java.util.Scanner;
+
+public class while_loop {
+    public static void main(String[] args)
+    {
+        int num;
+        System.out.print("Enter any Number: ");
+        Scanner scan=new Scanner(System.in);
+        num=scan.nextInt();
+
+        while(num>=0)
+        {
+            if(num%2==0)
+            {
+                System.out.println("Even Number");
+                break;
+            }
+            else
+            {
+                System.out.println("Odd Number");
+                break;
+            }
+        }
+    }
+    
+}
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,1 @@
+Alphabet_Check
