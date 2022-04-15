35b3320c42ea6bae20d145c7fe20eb6e897c790c
AMIT RAJBHAR <rajbharamit38@gmail.com>
2022-04-15 22:24:34 +0530

    calculate the sum of first 10 natural number

---
+++ Java loop/sum_natural_no.java
@@ -0,0 +1,12 @@
+public class sum_natural_no {
+    public static void main(String[] args)
+    {
+        int sum=0;
+        for(int i=1; i<=10; i++)
+        {
+            sum +=i;
+        }
+        System.out.println("Sum: "+sum);
+    }
+    
+}
