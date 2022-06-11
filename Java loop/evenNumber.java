c9ba3904afc9e65df430f215868cbce93cbef94a
AMIT RAJBHAR <rajbharamit38@gmail.com>
2022-06-12 01:48:35 +0530

    even Number Print

---
+++ Java loop/evenNumber.java
@@ -0,0 +1,17 @@
+
+
+public class evenNumber {
+    public static void main(String [] args )
+    {
+        int n=25;
+        for(int i=1; i<=25; i++)
+        {
+            if(i % 2==0)
+        {
+            System.out.println(i);
+        }
+    }
+
+    }
+    
+}
